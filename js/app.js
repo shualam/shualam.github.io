79c8e477788387e8b4c1b8ddc385f3eb9d3fc162
Shuyebul Alam <shualam@gmail.com>
2018-10-22 23:02:05 -0400

    update
    Added count down timer

--- js/app.js
+++ js/app.js
@@ -18,3 +18,32 @@ if (getHour >= 18){
 // grab the dom element and insert message
 let displayMessage = document.querySelector("#greeting");
 displayMessage.textContent = message;
+
+// Count down timer
+// Set the launch date
+let launchDate = new Date("November 30, 2018 12:00:00").getTime();
+
+// Count down every seconds
+let time = setInterval(function(){
+	
+// 	get current date and time
+	let currentTime = new Date().getTime();
+	
+// 	Calculate the launch date from today
+	let launchDay = launchDate - currentTime;
+	
+// 	calculate time for days, hour, minues, and seconds
+	let days = Math.floor(launchDay / (1000 * 60 * 60 * 24));
+	let hours = Math.floor((launchDay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	let minutes = Math.floor((launchDay % (1000 * 60 * 60)) / (1000 * 60));
+	let seconds =  Math.floor((launchDay % (1000 * 60)) / 1000);
+	
+// 	Display result in the website
+	let displayTimer = document.getElementById("countDownTimer").textContent = days + "D " + hours + "H " + minutes + "M " + seconds + "S ";
+	
+// 	When the launch day is finish, display a message
+	if (launchDay < 0) {
+		clearInterval(time);
+		document.getElementById("countDownTimer").textContent = "Launching soon!"
+	}
+}, 1000);
